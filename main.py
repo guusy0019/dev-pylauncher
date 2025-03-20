e18b91e1fd8cfd60ac5e806750ef0207f09e6012
guusy0019 <mattyan0915@gmail.com>
2025-03-20 17:17:50 +0900

    uvに移行logging設定

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import yaml
+import logging
+import logging.config
+
+from app.config.settings import LOG_CONFIG_PATH
 from app.ui.layout.layout import AppLayout
 from app.utility.app_manager import set_app
 
+with open(LOG_CONFIG_PATH, "r") as f:
+    log_config = yaml.safe_load(f)
+
+logging.config.dictConfig(log_config)
+
 def main():
+
     app = AppLayout()
     set_app(app)
     app.mainloop()
 
 if __name__ == "__main__":
     main()
+
