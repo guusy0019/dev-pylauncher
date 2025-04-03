083ef78c9586aafe8a27a1c27c9e6adbc6a0e612
guusy0019 <mattyan0915@gmail.com>
2025-04-03 22:28:27 +0900

    pytestでまたパスがみつからないエラーはいたのでその対策

--- app/ui/layout/layout.py
+++ app/ui/layout/layout.py
@@ -1,27 +1,82 @@
 import os
 import customtkinter
 from PIL import Image
-from app.config.settings import IMAGE_DIR
+from app.config.settings import IMAGE_DIR, USER_DATA_PATH
 from app.config.color_settings import (
     TEXT_COLOR,
     HOVER_COLOR,
     FG_COLOR,
 )
+from app.config.settings import MODE
+from app.utility.i18n import I18n
 from app.ui.layout._base_ctk_layout import BaseCtkLayout
 from app.ui.page.home_page import HomePage
 from app.ui.page.launcher_page import LauncherPage
 from app.ui.page.config_page import ConfigPage
-from app.config.settings import MODE
-from app.utility.i18n import I18n
+from app.ui.layout.menu_layout import MenuLayout
+from app.ui.widget.workspace_frame_widget import WorkspaceFrameWidget
+from app.ui.widget.appearance_mode_widget import AppearanceModeWidget
+from app.ui.widget.scaling_option_widget import ScalingOptionWidget
+from app.ui.widget.themes_color_widget import ThemesColorWidget
+from app.module.application.usecase.userdata_usecase import UserDataUsecase
+from app.module.infrastructure.repository.userdata_repository import UserDataRepository
 
 class AppLayout(BaseCtkLayout):
     def __init__(self):
+        # テーマカラーを設定していた場合それを一番最初に読み込みアプリ全体に反映させる
+        userdata_usecase = UserDataUsecase(userdata_repository=UserDataRepository())
+        user_data = userdata_usecase.get_all_user_data(user_data_path=USER_DATA_PATH)
+        
+        theme_color = user_data.get("theme_color")
+        if theme_color:
+            ThemesColorWidget.set_color_theme(theme_color)
+
+        appearance_mode = user_data.get("appearance_mode")
+        if appearance_mode:
+            AppearanceModeWidget.set_appearance_mode(appearance_mode)
+
+        scaling_option = user_data.get("scaling_option")
+        if scaling_option:
+            ScalingOptionWidget.set_scaling(scaling_option)
         super().__init__()
+
+        self.grid_columnconfigure(1, weight=1)
         self.i18n = I18n()  # 多言語対応のインスタンスを作成
 
         self.home_frame = HomePage(self)
         self.launcher_frame = LauncherPage(self)
         self.config_frame = ConfigPage(self)
+
+        # メニューバーを配置
+        self.menu_layout = MenuLayout(self)
+        self.menu_layout.grid(row=0, column=0, sticky="ew")
+
+        self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
+        self.navigation_frame.grid_rowconfigure(5, weight=1)
+
+        # ワークスペースのフレームを配置
+        self.workspace_frame = WorkspaceFrameWidget(self.navigation_frame)
+        self.workspace_frame.grid(row=5, column=0, sticky="nsew")
+
+        # テーマカラーのウィジェットの配置
+        self.themes_color_menu = ThemesColorWidget(self.navigation_frame)
+        # テーマカラーがすでに選択されていた場合はそれをoptionの初期値にする
+        if theme_color:
+            self.themes_color_menu.set(theme_color)
+        self.themes_color_menu.grid(row=9, column=0, padx=20, pady=(10, 20))
+
+        # 外観モードのウィジェットの配置
+        self.appearance_mode_menu = AppearanceModeWidget(self.navigation_frame)
+        if appearance_mode:
+            self.appearance_mode_menu.set(appearance_mode)
+        self.appearance_mode_menu.grid(row=7, column=0, padx=20, pady=(10, 20))
+
+        # スケーリングのウィジェットの配置
+        self.scaling_optionemenu = ScalingOptionWidget(self.navigation_frame)
+        if scaling_option:
+            self.scaling_optionemenu.set(scaling_option)
+        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
         # 将来的にここでworkspaceを管理できればいいかなと
         self.frames = {
